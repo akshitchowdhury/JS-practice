aae528446e0cbe67e536d44275f3b72871634fe6
akshitchowdhury <akshitchowdhury@gmai.com>
2023-02-07 21:07:46 +0530

    first new file:   js_prac/class_tets.js

---
+++ js_prac/class_tets.js
@@ -0,0 +1,17 @@
+
+class anime{
+
+constructor(alias){
+this.alias = alias;
+
+}
+
+  display(){
+    console.log("Name of the anime"+" "+ this.alias)
+  }
+
+}
+
+var a1 = new anime("AOT");
+
+a1.display();
